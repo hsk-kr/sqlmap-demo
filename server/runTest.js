6f02e647ff036f9ae753ec681f4655648a0498d7
hsk-kr <hsk.coder@gmail.com>
2024-01-31 21:00:01 +0100

    test: update runTest logic
    - before:
      It decided whether there was an error ot not by parsing string, which is inefficient and affected by test output changes.
    - after:
      catches exit code from an executed command and calles `process.exit` with the code.

--- server/runTest.js
+++ server/runTest.js
@@ -1,64 +1,14 @@
 const { spawn } = require('child_process');
 
-const isTestResult = (text) => {
-  /**
-   * Test Text Example:
-   * Test Suites: 2 passed, 2 total
-   * Tests:       14 passed, 14 total
-   * Snapshots:   0 total
-   * Time:        5.126 s
-   * Ran all test suites.
-   */
-  const trimText = text.trim();
-
-  return (
-    trimText.startsWith('Test Suites:') &&
-    trimText.endsWith('Ran all test suites.')
-  );
-};
-
-const isTestPass = (text) => {
-  /**
-   * Test Text Example:
-   * Test Suites: 2 passed, 2 total
-   * Tests:       14 passed, 14 total
-   * Snapshots:   0 total
-   * Time:        5.126 s
-   * Ran all test suites.
-   */
-
-  let testText = text.split('Tests:');
-  testText = testText[1].split('Snapshots:');
-  testText = testText[0].split(',');
-
-  const result = { failed: 0, passed: 0, total: 0 };
-
-  for (const str of testText) {
-    const [count, label] = str.trim().split(' ');
-
-    result[label] = Number(count);
-  }
-
-  return result.total === result.passed;
-};
-
 const runTestScriptIfServerIsReady = (url) => {
   const tm = setInterval(async () => {
     try {
       await fetch(url);
-
       clearInterval(tm);
 
       const test = spawn('npm', ['run', 'test']);
-      let error = false;
 
-      const printAndCheckAllTestPass = (data) => {
-        console.log(data);
-
-        if (isTestResult(data)) {
-          error = !isTestPass(data);
-        }
-      };
+      const printAndCheckAllTestPass = (data) => console.log(data);
 
       test.stdout.setEncoding('utf8');
       test.stdout.on('data', printAndCheckAllTestPass);
@@ -66,11 +16,7 @@ const runTestScriptIfServerIsReady = (url) => {
       test.stderr.setEncoding('utf8');
       test.stderr.on('data', printAndCheckAllTestPass);
 
-      test.on('close', () => {
-        if (error) {
-          process.exit(1);
-        }
-      });
+      test.on('exit', (code) => process.exit(code));
     } catch {}
   }, 1000);
 };
